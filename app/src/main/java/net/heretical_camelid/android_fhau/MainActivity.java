f4e066f782a409e92af9b588b44232cb93c53101
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-12-04 17:02:10 +0800

    App now sends all messages from 24/11 capture session which have single-frame responses
    Amp switches voices and buzzes a lot

--- app/src/main/java/net/heretical_camelid/android_fhau/MainActivity.java
+++ app/src/main/java/net/heretical_camelid/android_fhau/MainActivity.java
@@ -59,6 +59,25 @@ public class MainActivity extends AppCompatActivity {
 
         String[] commandHexStrings = new String[]{
             "35:09:08:00:8a:07:04:08:00:10:00",
+                "35:07:08:00:b2:06:02:08:01",
+                // Next 60 + 1 commands get
+                // the JSON descriptions of the 60 stored
+                // and 1 active presets
+                // We can't handle these until we have support
+                // for multi-frame responses
+                //"35:07:08:00:ca:06:02:08:01",
+                //"35:07:08:00:ca:06:02:08:02",
+                //..
+                "35:07:08:00:f2:03:02:08:01",
+                "35:07:08:00:d2:06:02:08:01",
+                "35:07:08:00:e2:06:02:08:01",
+                "35:07:08:00:d2:0c:02:08:01",
+                "35:09:08:00:8a:07:04:08:01:10:00",
+                "35:07:08:00:8a:02:02:08:02",
+                "35:07:08:00:8a:02:02:08:03",
+                "35:07:08:00:8a:02:02:08:04",
+                "35:07:08:00:8a:02:02:08:01",
+                "35:07:08:00:8a:02:02:08:02",
         };
         /*
         for(int i=0; i<commandHexStrings.length; ++i)
@@ -105,6 +124,11 @@ public class MainActivity extends AppCompatActivity {
                     sb.append("Sent " + commandHexStrings[i] + "\n");
                     sb.append("Received " + ByteArrayTranslator.bytesToHex(responseBytes) + "\n");
                     report_tv.setText(sb.toString());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        sb.append("sleep interrupted\n");
+                    }
                 }
 
                 /*
