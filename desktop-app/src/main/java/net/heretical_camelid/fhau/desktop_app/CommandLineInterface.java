1651ed4654193212c652e1e7650a88dd87e8fbda
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-12-11 13:47:16 +0800

    Added basic command line application

---
+++ desktop-app/src/main/java/net/heretical_camelid/fhau/desktop_app/CommandLineInterface.java
@@ -0,0 +1,8 @@
+package net.heretical_camelid.fhau.desktop_app;
+
+public class CommandLineInterface {
+    static public void main(String[] args) {
+        System.out.println("Hello world");
+        System.exit(0);
+    }
+}
