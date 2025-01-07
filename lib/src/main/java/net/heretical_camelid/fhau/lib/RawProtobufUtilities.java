55e16cae3a7ade84fab7af24e92d22c41b009690
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-01-07 15:56:06 +0800

    Added initial implementation of RPU.extractVarint and its test

--- lib/src/main/java/net/heretical_camelid/fhau/lib/RawProtobufUtilities.java
+++ lib/src/main/java/net/heretical_camelid/fhau/lib/RawProtobufUtilities.java
@@ -2,6 +2,8 @@ package net.heretical_camelid.fhau.lib;
 
 //enum MustangProtobufDialect { MPD_GUESS,  MPD_LT40S,  MPD_MMP };
 public class RawProtobufUtilities {
+
+
     public static final int getMainMessageTag(byte[] pbuf_array) {
         // assert mpd==MustangProtobufDialect.MPD_LT40S;
         return 0;
@@ -17,25 +19,27 @@ public class RawProtobufUtilities {
         }
         return retval;
     }
-    private int extractVarint(byte[] pbuf_array, int viBounds[]) {
+    private static int extractVarint(byte[] pbuf_array, int viBounds[]) {
         assert viBounds[0] == viBounds[1];
         assert viBounds[0] < pbuf_array.length;
         int viValue = 0;
-        while( ( 0x10&pbuf_array[viBounds[1]] ) != 0x00 ) {
-            viValue <<= 7;
-            viValue += (0x7F&pbuf_array[viBounds[1]]);
-            ++viBounds[1];
+        int itemMultiplier = 1;
+        while( ( ((byte)0x80)&pbuf_array[viBounds[1]] ) != (byte)0x00 ) {
+            viValue += itemMultiplier * (0x7F&pbuf_array[viBounds[1]]);
+            viBounds[1]+=1;
+            itemMultiplier <<=7;
         }
-        viValue <<= 7;
-        viValue += pbuf_array[viBounds[1]];
-        ++viBounds[1];
+        viValue += itemMultiplier*pbuf_array[viBounds[1]];
+        viBounds[1]+=1;
         return viValue;
     }
 
     public static void main(String[] args) {
+        final String TEST_MESSAGE = "08008a07040800100000";
         try {
+
             System.out.println("Checking hexToBytes");
-            byte[] h2bResult = hexToBytes("08008a07040800100000+");
+            byte[] h2bResult = hexToBytes(TEST_MESSAGE);
             byte[] h2bExpected = new byte[]{
                 0x08, 0x00, (byte) 0x8a, 0x07, 0x04, 0x08, 0x00, 0x10, 0x00, 0x00
             };
@@ -49,6 +53,26 @@ public class RawProtobufUtilities {
                 );
             }
             System.out.println("hexToBytes ok");
+
+            System.out.println("Checking extract_varint");
+            // Attempting to interpret the varint 8a 07 starting at
+            // offset 2 into the message
+            int viBounds[] = { 2, 2 };
+            int evResult = extractVarint(h2bExpected, viBounds);
+            int evExpected = 906;
+            assert evResult == evExpected: String.format(
+                "extractVarint returned unexpected value (actual:%d, expected:%d)",
+                evResult,evExpected
+            );
+            // We expect that viBounds[1] has been updated as a side effect to reflect the
+            // number of bytes consumed to decode the varint
+            int viBounds1Expected = 4;
+            assert viBounds[1] == viBounds1Expected: String.format(
+                "extractVarint viBounds[1] unexpected value (actual:%d, expected:%d)",
+                viBounds[1],viBounds1Expected
+            );
+
+            System.out.println("extractVarint ok");
 
         }
         catch(Exception e) {
