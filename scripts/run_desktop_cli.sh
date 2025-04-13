07ce2e207afdf074daea4261ede2f64f6dac6a2b
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-04-13 10:45:46 +0800

    Run script can be much simpler now jar is executable

--- scripts/run_desktop_cli.sh
+++ scripts/run_desktop_cli.sh
@@ -1,31 +1,7 @@
 #! /bin/bash
 
-# This is a quick and dirty script to run the application for
-# which the main class is
-# desktop-app/src/main/java/net/heretical_camelid/fhau/desktop-app/CommandLineInterface.java
-
-# Exit without running 'java' if 'javac' fails (bash behaviour)
+# This is a quick and dirty script to build and run the FHAU command line application
 set -e
-
-debug_echo() {
-  # Comment the return statement to reveal messages posted via this function
-  #return
-  echo $*
-}
-
-CLASSPATH="desktop-app/build/libs/desktop-app.jar"
-
-export CLASSPATH
-echo $CLASSPATH
-
-if [ ! -z "$*" ]
-then
-  java -cp $CLASSPATH -ea  net.heretical_camelid.fhau.desktop_app.CommandLineInterface $*
-else
-  tsdir=_work/cli-$(date +%s)
-  mkdir $tsdir
-  java -cp $CLASSPATH -ea  net.heretical_camelid.fhau.desktop_app.CommandLineInterface $tsdir
-fi
-
-
-
+./gradlew build
+cd _work
+java -jar ../desktop-app/build/libs/desktopFHAUcli-0.0.0.jar $*
