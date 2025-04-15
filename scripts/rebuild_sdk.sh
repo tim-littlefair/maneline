b0e6eaeabbfe5db9486d37da4e51f47e358a0a8b
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-04-15 14:47:24 +0800

    Tweaking SDK creation

--- scripts/rebuild_sdk.sh
+++ scripts/rebuild_sdk.sh
@@ -1,37 +1,70 @@
 #!/bin/sh
 
-# The development environment can be anywhere but it 
-# is recommended it should sit as a sibling directory 
-# alongside the root of the Git repository so that
-# a) there is no danger of accidentally checking the 
-# development environment in; and
-# b) development environment tools can be run manually
-# from a shell in the root of the repository with 
-# simple consistent command lines.
+# The purpose of this script is to build a composite SDK for
+# FHAU development, which consists of the following components:
+# + a vendor Java Development Kit
+# + a vendor Android SDK
+# + the user preferences directory where the Android SDK
+#   stores its settings
+# + a gradle user home directory where gradle packages
+#   are cached after first use.
 
-devenv_reldir=../$SDK_NAME
+# The FHAU SDK could be anywhere but it is recommended
+# it should sit as a sibling directory alongside the
+# root of the Git repository so that
+# a) there is no danger of accidentally checking the 
+# SDK in; and
+# b) SDK tools can be run manually from a shell in the
+# root of the repository using command lines based on
+# easy to remember relative paths.
+# This script enforces that recommendation by accepting
+# the path relative to the parent of the Git repo as
+# its parameter.
+
+if [ ! -z "$1" ]
+then
+    SDK_NAME=$1
+    echo SDK name from command line argument: $SDK_NAME
+elif [ ! -z "$SDK_NAME" ]
+then
+    SDK_NAME=$1
+    echo SDK name from environment: $SDK_NAME
+    # Environment variable is intended for use in a CI
+    # job where a new SDK is built from scratch
+    # before building the FHAU project itself
+else
+    echo No value found for SDK_NAME
+    exit 1
+fi
+
+# We expect the run to start in the root directory
+# of the Git repository
+repo_dir=$(pwd)
+
+fhau_sdk_reldir=../$SDK_NAME
 
 # The script expects that when it runs, the 
 # development environment directory will not exist...
-if [ -e $devenv_reldir ]
+if [ -e $fhau_sdk_reldir ]
 then
-  echo There is either a file or a directory at $devenv_reldir.
+  echo There is either a file or a directory at $fhau_sdk_reldir.
   echo Please check the content of this location and remove
   echo it manually before running this script again.
-  # exit 1
+  exit 2
 fi
 
 # ... but its parent will
-mkdir $devenv_reldir
+mkdir $fhau_sdk_reldir
 if [ ! "$?" = "0" ]
 then
-  echo Unable to create devenv at $devenv_reldir
-  # exit 1
+  echo Unable to create devenv at $fhau_sdk_reldir
+  exit 3
 fi
+cd $fhau_sdk_reldir
+fhau_sdk_absdir=$(pwd)
+echo Absolute path to SDK is $fhau_sdk_absdir
 
 # ... and a download cache might or might not
-cd $devenv_reldir
-devenv_absdir=$(pwd)
 cache_reldir=../cache
 if [ ! -d $cache_reldir ]
 then
@@ -39,7 +72,7 @@ then
   if [ ! "$?" = "0" ]
   then
     echo Unable to create cache
-    exit 2
+    exit 4
   fi
 fi
 cd $cache_reldir
@@ -83,7 +116,7 @@ then
   curl $android_cltools_url -o $android_cltools_file
 fi
 
-cd $devenv_absdir
+cd $fhau_sdk_absdir
 pwd
 
 echo Unpacking $jdk_file
@@ -97,33 +130,51 @@ mv cmdline-tools latest
 mkdir cmdline-tools
 mv latest cmdline-tools/latest
 
-cd $devenv_absdir
+cd $fhau_sdk_absdir
 
-if [ -d $devenv_absdir/jdk-21.0.2 ]
+if [ -d $fhau_sdk_absdir/jdk-21.0.2 ]
 then
-  export JAVA_HOME=$devenv_absdir/jdk-21.0.2
-elif [ -d $devenv_absdir/jdk-21.0.2.jdk/Contents/Home ]
+  export JAVA_HOME=$fhau_sdk_absdir/jdk-21.0.2
+elif [ -d $fhau_sdk_absdir/jdk-21.0.2.jdk/Contents/Home ]
 then
-  export JAVA_HOME=$devenv_absdir/jdk-21.0.2.jdk/Contents/Home
+  export JAVA_HOME=$fhau_sdk_absdir/jdk-21.0.2.jdk/Contents/Home
 else
   echo Could not find appropriate JAVA_HOME
-  exit 2
+  exit 5
 fi
-ANDROID_HOME=$devenv_absdir/android-sdk
+ANDROID_HOME=$fhau_sdk_absdir/android-sdk
 
-cat > sdk_vars.sh <<+
+cat > $fhau_sdk_absdir/fhau_sdk_vars.sh <<+
+
 JAVA_HOME=$JAVA_HOME
 ANDROID_HOME=$ANDROID_HOME
 ANDROID_USER_HOME=$ANDROID_HOME
-GRADLE_USER_HOME=$devenv_absdir/gradle-user-home
-PATH=$JAVA_HOME/bin:$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
+GRADLE_USER_HOME=$fhau_sdk_absdir/gradle-user-home
+GRADLE_LOCAL_JAVA_HOME=$JAVA_HOME
+PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$JAVA_HOME/bin:$PATH
+
+export JAVA_HOME ANDROID_HOME ANDROID_USER_HOME GRADLE_USER_HOME GRADLE_LOCAL_JAVA_HOME PATH
+
 +
 
-. sdk_vars.sh
+# We need to overwrite a non-version-controlled file in the root
+# directory of the repository to ensure the SDK is found by Gradle
+cat > $repo_dir/local.properties <<+
 
+# local.properties file overwritten by $0 on $(date --iso-8601)
+sdk.dir=$fhau_sdk_absdir
+
++
+
+. $fhau_sdk_absdir/fhau_sdk_vars.sh
+
+# I haven't yet found a way of caching the Android SDK download packages
+# Also, Gradle seems to need more than one version of build-tools
+# to complete the first build
 yes | sdkmanager --install \
-  "build-tools;35.0.1" \
   "build-tools;34.0.0" \
+  "build-tools;35.0.0" \
+  "build-tools;35.0.1" \
   "platform-tools" \
   "emulator" \
   "sources;android-35" \
