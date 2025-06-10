c88a4e7981b2bc84e4970eb6cb70373cb7dc874f
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-06-10 16:52:45 +0800

    Audio graph survey script complete for now

--- scripts/audio_graph_survey.py
+++ scripts/audio_graph_survey.py
@@ -11,23 +11,26 @@ def scan_dir(dir):
     for f in sorted(os.listdir(dir)):
         if f.endswith(".raw_preset.json"):
             ps = json.load(open(os.path.join(dir,f)))
-            connections = ps["audioGraph"]["connections"]
+
+            # root_order tells us the order of keys at the
+            # root of the JSON tree.
+            # This order has no meaning, but may be a clue to
+            # where/how the preset was last edited (i.e. whether
+            # it is
+            # + a default preset unmodified since the firmware
+            #   state was last factory preset; or
+            # + a preset which has been edited on the device
+            # + a preset which has been imported using FenderTone
+            # )
+            root_order = "".join([key[0] for key in ps.keys()])
             nodes = ps["audioGraph"]["nodes"]
-            cxn_order = []
-            for connection in connections:
-                input = connection["input"]
-                output = connection["output"]
-                cxn_order += [
-                    input["nodeId"][0] + str(input["index"]) +
-                    output["nodeId"][0] + str(output["index"])
-                ]
             node_order = ""
             for node in nodes:
                 nodeId = node["nodeId"][0]
                 if node["FenderId"] != "DUBS_Passthru":
                     nodeId=nodeId.upper()
                 node_order += nodeId
-            print(node_order, ".".join(cxn_order),f)
+            print(root_order,node_order,f)
         else:
             pass
 
