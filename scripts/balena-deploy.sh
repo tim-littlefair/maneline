26b50443c26d76f900921d5c32edd415fa9702aa
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-07-30 13:27:34 +0800

    Deploy now sends UI to browser

--- scripts/balena-deploy.sh
+++ scripts/balena-deploy.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
+if [ "$1" = "--no-browser" ]
+then
+    no_browser_mode=1
+    shift
+fi
+
 if [ ! -z "$1" ]
 then
     target=$1
-else
+elif [ ! -z "$FHAU_DEPLOY_TARGET" ]
+then
     target=$FHAU_DEPLOY_TARGET
+else
+    echo The target device must be specified
+    exit 1
 fi
 
 echo Deploying to $target
 
-balena push $target --detached --source deployment/balena --env tl-fhau-web:LOCAL_BROWSER=0 --service tl-fhau-web
+if [ "$no_browser_mode" = "1" ]
+then
+    balena push $target --detached --source deployment/balena --env tl-fhau-web:LOCAL_BROWSER=0 --service tl-fhau-web
+else
+    balena push $target --detached --source deployment/balena
+fi
+
