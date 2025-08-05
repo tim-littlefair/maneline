1bec5d1ee243a9c97cb8cd3f72b5006a2290bb87
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-08-05 17:29:33 +0800

    WIP on stdin/pegasus event loop

---
+++ deployment/balena/lua/fhau_eventloop.lua
@@ -0,0 +1,82 @@
+#! lua
+
+function _debug_mark(c)
+    io.stdout:write(c)
+    io.stdout:flush()
+end
+
+pegasus_handler = require 'pegasus.handler'
+local pegasus_sock = nil
+function get_pegasus_fd(port)
+    socket = require 'socket'
+    pegasus_sock = socket.bind('*', port)
+    assert(pegasus_sock)
+    function callback(req,res)
+        _debug_mark("|gps_cb|")
+        return res:close()
+    end
+    local hdlr = pegasus_handler:new(callback, ".")
+    return pegasus_sock
+end
+
+function get_stdin_fd()
+    socket = require 'socket'
+    fake_stdin = socket.tcp()
+    fake_stdin:close()
+    fake_stdin:setfd(0)
+    fake_stdin:settimeout(0.1)
+    return fake_stdin
+end
+
+socket = require 'socket'
+
+fd_stdin = get_stdin_fd()
+fd_pegasus = get_pegasus_fd(9090)
+_ACTIVE_TIMEOUT = 0.1
+_PASSIVE_TIMEOUT = 1.0
+timeout=_ACTIVE_TIMEOUT
+while(1)
+do
+    fd_pegasus:settimeout(timeout)
+    fd_stdin:settimeout(timeout)
+    fd_array = { fd_stdin }
+    pegasus_client = fd_pegasus:accept()
+    if(pegasus_client)
+    then
+        fd_array[#fd_array+1] = pegasus_client
+    end
+    select_rv = socket.select(fd_array,nil, timeout)
+    if(#select_rv==0)
+    then
+        timeout=_PASSIVE_TIMEOUT
+        _debug_mark(".")
+    elseif(select_rv[1]==pegasus_client)
+    then
+        timeout=_ACTIVE_TIMEOUT
+        print("pegasus",select_rv[1]:read())
+    elseif(select_rv[1]:getfd()==0)
+    then
+        stdin_bytes = io.stdin:read()
+        if(stdin_bytes)
+        then
+            timeout=_ACTIVE_TIMEOUT
+            print("stdin:",stdin_bytes)
+        else
+            timeout=_PASSIVE_TIMEOUT
+            _debug_mark("#")
+        end
+    else
+            timeout=_PASSIVE_TIMEOUT
+            _debug_mark("!")
+    end
+end
+
+--[[
+  copas.addserver(server_sock, copas.handler(function(client_sock)
+    hdlr:processRequest(server_port, client_sock)
+  end, opts.sslparams))
+
+  hdlr.log:info('Pegasus is up on %s://%s:%s', opts.sslparams and "https" or "http", server_ip, server_port)
+  return server_sock
+require 'cqueues'
+]]
