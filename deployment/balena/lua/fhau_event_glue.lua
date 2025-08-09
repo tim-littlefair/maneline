939693469ccb120ee5c238d211515983121f3324
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-08-09 15:03:04 +0800

    Loop is handing both stdin and http inputs

--- deployment/balena/lua/fhau_event_glue.lua
+++ deployment/balena/lua/fhau_event_glue.lua
@@ -5,17 +5,15 @@ function _debug_mark(c)
     io.stdout:flush()
 end
 
-local pegasus_sock = nil
 function get_pegasus_event_client(port)
     local socket = require 'socket'
     pegasus_handler = require 'pegasus.handler'
 
-    retval = {}
+    local retval = {}
     retval._port = port
-    retval._socket = socket.bind('*', port)
-    assert(retval._socket)
+    retval._socket, err = socket.bind('*', port)
+    assert(retval._socket, err)
     function callback(req,res)
-        _debug_mark("|gps_cb|")
         res:write("HW!")
         return res:close()
     end
@@ -35,73 +33,77 @@ function get_pegasus_event_client(port)
     function retval:handler()
         assert(self._client~=nil)
         self._phdlr:processRequest(self._port,self._client)
-        first_line = "XXX" -- self._socket.read("*line")
         self._client:close()
         self._client=nil
-        return first_line
     end
     return retval
 end
 
-function get_stdin_fd()
+function get_stdin_event_client()
     socket = require 'socket'
-    fake_stdin = socket.tcp()
-    fake_stdin:close()
-    fake_stdin:setfd(0)
-    fake_stdin:settimeout(0.1)
-    return fake_stdin
+
+    local retval = {}
+    retval._socket = socket.tcp()
+    retval._socket:close()
+    retval._socket:setfd(0)
+    retval._socket:settimeout(0.1)
+    function retval:fd()
+        return self._socket
+    end
+    function retval:settimeout(t)
+        return self._socket:settimeout(t)
+    end
+    function retval:handler()
+        return nil
+    end    
+    return retval
 end
 
-socket = require 'socket'
+function run_event_loop(stdin_evtclt, pegasus_evtclt)
+    socket = require 'socket'
 
-fd_stdin = get_stdin_fd()
-pegasus_evclt = get_pegasus_event_client(9090)
-_ACTIVE_TIMEOUT = 0.1
-_PASSIVE_TIMEOUT = 1.0
-timeout=_ACTIVE_TIMEOUT
-while(1)
-do
-    fd_stdin:settimeout(timeout)
-    fd_array = { fd_stdin }
-    pegasus_client_fd = pegasus_evclt:fd()
-    if(pegasus_client_fd)
-    then
-        pegasus_client_fd:settimeout(timeout)
-        fd_array[#fd_array+1] = pegasus_client_fd
-    end
-
-    _debug_mark(#fd_array)
-
-    select_rv = socket.select(fd_array,nil, timeout)
-    if(#select_rv==0)
-    then
-        timeout=_PASSIVE_TIMEOUT
-        _debug_mark(".")
-    elseif(select_rv[#select_rv]==pegasus_client_fd)
-    then
-        _debug_mark("<")
-        timeout=_ACTIVE_TIMEOUT
-        print("pegasus",pegasus_evclt:handler())
-        _debug_mark(">")
-    else
-        stdin_bytes = io.stdin:read("*line")
-        if(stdin_bytes)
+    _ACTIVE_TIMEOUT = 0.1
+    _PASSIVE_TIMEOUT = 1.0
+    timeout=_ACTIVE_TIMEOUT
+    while(1)
+    do
+        stdin_evtclt:settimeout(timeout)
+        fd_array = { stdin_evtclt:fd() }
+        pegasus_client_fd = pegasus_evtclt:fd()
+        if(pegasus_client_fd)
         then
-            timeout=_ACTIVE_TIMEOUT
-            print("stdin:",stdin_bytes)
-        else
+            pegasus_client_fd:settimeout(timeout)
+            fd_array[#fd_array+1] = pegasus_client_fd
+        end
+
+        select_rv = socket.select(fd_array, nil, timeout)
+        if(#select_rv==0)
+        then
+            _debug_mark("!")
             timeout=_PASSIVE_TIMEOUT
-            _debug_mark("#")
+        elseif(select_rv[#select_rv]==pegasus_client_fd)
+        then
+            _debug_mark("p<")
+            pegasus_evtclt:handler()
+            timeout=_ACTIVE_TIMEOUT
+            _debug_mark(">\n")
+        else
+            stdin_bytes = io.stdin:read("*line")
+            if(stdin_bytes)
+            then
+                _debug_mark("s<")
+                print(stdin_bytes)
+                _debug_mark(">\n")
+                timeout=_ACTIVE_TIMEOUT
+            else
+                timeout=_PASSIVE_TIMEOUT
+            end
         end
     end
 end
 
---[[
-  copas.addserver(server_sock, copas.handler(function(client_sock)
-    hdlr:processRequest(server_port, client_sock)
-  end, opts.sslparams))
+local _stdin_evtclt = get_stdin_event_client()
+local _pegasus_evtclt = get_pegasus_event_client(9090)
 
-  hdlr.log:info('Pegasus is up on %s://%s:%s', opts.sslparams and "https" or "http", server_ip, server_port)
-  return server_sock
-require 'cqueues'
-]]
+run_event_loop(_stdin_evtclt, _pegasus_evtclt)
+
