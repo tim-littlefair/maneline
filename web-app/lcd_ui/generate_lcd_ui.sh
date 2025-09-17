fe9d46949a19e55b791f04373ad45b22c7424f51
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-09-17 15:28:51 +0800

    Interposing of icon and QR code now working

--- web-app/lcd_ui/generate_lcd_ui.sh
+++ web-app/lcd_ui/generate_lcd_ui.sh
@@ -13,6 +13,12 @@ psmodule3=Twin65
 psmodule4=MonoDelay
 psmodule5=LargeHallReverb
 
+icon_data_base64=$(base64 --wrap=0 < assets/maneline-icon.png)
+icon_data_uri="data:image/png;base64,$icon_data_base64"
+qrcode_data_base64=$(zint -b QRCODE -d http://$ipaddress:8080 --filetype=PNG --direct | base64 --wrap=0)
+qrcode_data_uri="data:image/png;base64,$qrcode_data_base64"
+
+
 svg_text=$(
   cat $template |
   sed -e "s/##/$psslot/" |
@@ -25,7 +31,9 @@ svg_text=$(
   sed -e "s/@psmodule2/$psmodule2/" |
   sed -e "s/@psmodule3/$psmodule3/" |
   sed -e "s/@psmodule4/$psmodule4/" |
-  sed -e "s/@psmodule5/$psmodule5/"
+  sed -e "s/@psmodule5/$psmodule5/" |
+  sed -e "s^@icon_data_uri^$icon_data_uri^" |
+  sed -e "s^@qrcode_data_uri^$qrcode_data_uri^"
 )
 
 echo $svg_text | rsvg-convert - -o lcd.png
