7f75c33a81dc802cf6ebd8edab3a39f026624459
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-09-19 16:40:27 +0800

    Generating mockup and splash image

--- web-app/lcd_ui/generate_lcd_ui.sh
+++ web-app/lcd_ui/generate_lcd_ui.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+generate_svg_text () {
+  cat $template |
+  sed -e "s/##/$psslot/" |
+  sed -e "s/@ipaddress/$ipaddress/" |
+  sed -e "s/@swversion/$swversion/" |
+  sed -e "s/@ampname/$ampname/" |
+  sed -e "s/@fwversion/$fwversion/" |
+  sed -e "s/@psname1/$psname1/" |
+  sed -e "s/@psname2/$psname2/" |
+  sed -e "s/@psmodule1/$psmodule1/" |
+  sed -e "s/@psmodule2/$psmodule2/" |
+  sed -e "s/@psmodule3/$psmodule3/" |
+  sed -e "s/@psmodule4/$psmodule4/" |
+  sed -e "s/@psmodule5/$psmodule5/" |
+  sed -e "s^@icon_data_uri^$icon_data_uri^" |
+  sed -e "s^@qrcode_data_uri^$qrcode_data_uri^"
+}
+
 template=$(dirname $0)/lcd-480x320-template.svg
 ipaddress=$(ip addr | grep -e wlan0 -e wlp3s0 | grep inet | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | head -1)
 swversion=v0.2.0+beta99
@@ -19,31 +37,27 @@ icon_data_uri="data:image/png;base64,$icon_data_base64"
 qrcode_data_base64=$(zint -b QRCODE -d http://$ipaddress:8080 --filetype=PNG --direct | base64 --wrap=0)
 qrcode_data_uri="data:image/png;base64,$qrcode_data_base64"
 
-svg_text=$(
-  cat $template |
-  sed -e "s/##/$psslot/" |
-  sed -e "s/@ipaddress/$ipaddress/" |
-  sed -e "s/@swversion/$swversion/" |
-  sed -e "s/@ampname/$ampname/" |
-  sed -e "s/@fwversion/$fwversion/" |
-  sed -e "s/@psname1/$psname1/" |
-  sed -e "s/@psname2/$psname2/" |
-  sed -e "s/@psmodule1/$psmodule1/" |
-  sed -e "s/@psmodule2/$psmodule2/" |
-  sed -e "s/@psmodule3/$psmodule3/" |
-  sed -e "s/@psmodule4/$psmodule4/" |
-  sed -e "s/@psmodule5/$psmodule5/" |
-  sed -e "s^@icon_data_uri^$icon_data_uri^" |
-  sed -e "s^@qrcode_data_uri^$qrcode_data_uri^"
-)
-
-echo $svg_text | rsvg-convert - -o assets/lcd-mockup.png
+# Generate a mockup
+svg_text1=$(generate_svg_text)
+echo $svg_text1 | rsvg-convert - -o $(dirname $0)/lcd-mockup.png
 if [ ! "$?" = 0 ]
 then
   echo SVG Text:
-  echo $svg_text
+  echo $svg_text1
 fi
 
+# Generate a splash screen
+template=$(dirname $0)/lcd-480x320-splash.svg
+svg_text2=$(generate_svg_text)
+echo $svg_text2 | rsvg-convert - -o $(dirname $0)/lcd-startup.png
+if [ ! "$?" = 0 ]
+then
+  echo SVG Text:
+  echo $svg_text2
+fi
 
-
+# BalenaCloud requires the startup image to be 13.09kb or less
+# so we need to minimize that image
+optipng $(dirname $0)/lcd-startup.png --clobber -o8 -out $(dirname $0)/lcd-startup-min.png
+ls -l $(dirname $0)/*.png
 
