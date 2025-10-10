a569d9575a78564808e83f97fed8734be8c22081
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-10-10 23:53:15 +0800

    Python file watches preset-details.json and updates

--- web-app/lcd_ui/generate_lcd_svg.py
+++ web-app/lcd_ui/generate_lcd_svg.py
@@ -12,6 +12,10 @@ import time
 import traceback
 import webbrowser
 
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.events import FileSystemEventHandler
+
 # Get local ip address
 # ref: https://stackoverflow.com/a/23822431
 def getNetworkIp():
@@ -65,6 +69,32 @@ def generate_qrcode_data_uri(ipaddr):
     qrcode_data_base64=generation_outcome.stdout
     return f"data:image/png;base64,{qrcode_data_base64}"
 
+class PresetUpdatedHandler(PatternMatchingEventHandler):
+    def __init__(self, run_dir, ipaddr):
+        super().__init__(
+            patterns = [ "./preset-details.json" ],
+            ignore_directories=True
+        )
+        self.run_dir = run_dir
+        self.ipaddr = ipaddr
+
+    def on_closed(self,event):
+        print(event)
+        update_lcd_ui(self.run_dir, self.ipaddr)
+
+
+def update_lcd_ui(run_dir, ipaddr):
+    device_details = json.load(open(os.path.join(run_dir, "amp-details.json")))
+    preset_details = json.load(open(os.path.join(run_dir, "preset-details.json")))
+    icon_data_uri = generate_icon_data_uri()
+    qrcode_data_uri = generate_qrcode_data_uri(ipaddr)
+    generate_svg(
+        svg_template_str, ipaddr,
+        device_details, preset_details,
+        icon_data_uri, qrcode_data_uri
+    )
+
+
 if __name__ == "__main__":
     try:
         run_dir=None
@@ -79,80 +109,23 @@ if __name__ == "__main__":
         ).read()
         ipaddr=getNetworkIp()
         session_dir = find_latest_session_dir(run_dir)
-        device_details = json.load(open(os.path.join(run_dir,"amp-details.json")))
-        preset_details = json.load(open(os.path.join(run_dir,"preset-details.json")))
-        icon_data_uri = generate_icon_data_uri()
-        qrcode_data_uri = generate_qrcode_data_uri(ipaddr)
-        generate_svg(
-            svg_template_str, ipaddr,
-            device_details, preset_details,
-            icon_data_uri, qrcode_data_uri
+        update_lcd_ui(run_dir, ipaddr)
+
+        preset_details_observer = Observer()
+        preset_details_observer.schedule(
+            PresetUpdatedHandler(run_dir, ipaddr),
+            f"{run_dir}",recursive=True,
         )
+        preset_details_observer.start()
+        try:
+            while(True):
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            pass
+        preset_details_observer.stop()
+        preset_details_observer.join()
+
 
     except:
         traceback.print_exc()
 
-r"""
-
-generate_svg_text () {
-  cat $template |
-  sed -e "s/##/$psslot/" |
-  sed -e "s/@ipaddress/$ipaddress/" |
-  sed -e "s/@swversion/$swversion/" |
-  sed -e "s/@ampname/$ampname/" |
-  sed -e "s/@fwversion/$fwversion/" |
-  sed -e "s/@psname1/$psname1/" |
-  sed -e "s/@psname2/$psname2/" |
-  sed -e "s/@psmodule1/$psmodule1/" |
-  sed -e "s/@psmodule2/$psmodule2/" |
-  sed -e "s/@psmodule3/$psmodule3/" |
-  sed -e "s/@psmodule4/$psmodule4/" |
-  sed -e "s/@psmodule5/$psmodule5/" |
-  sed -e "s^@icon_data_uri^$icon_data_uri^" |
-  sed -e "s^@qrcode_data_uri^$qrcode_data_uri^"
-}
-
-template=$(dirname $0)/lcd-480x320-template.svg
-ipaddress=$(ip addr | grep -e wlan0 -e wlp3s0 | grep inet | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | head -1)
-swversion=v0.2.0+beta99
-ampname="Mustang LT40S"
-fwversion="firmware 1.0.7"
-psslot=41
-psname1=EVENING
-psname2=RAGE
-psmodule1=s:SimpleCompressor
-psmodule2=m:TriangleFlanger
-psmodule3=a:Twin65
-psmodule4=d:MonoDelay
-psmodule5=r:LargeHallReverb
-
-
-
-# Generate a mockup
-svg_text1=$(generate_svg_text)
-echo $svg_text1 > $(dirname $0)/lcd-mockup.svg
-echo $svg_text1 | rsvg-convert - -o $(dirname $0)/lcd-mockup.png
-echo $svg_text1 | rsvg-convert - --zoom=2.5 -o $(dirname $0)/lcd-mockup-large.png
-if [ ! "$?" = 0 ]
-then
-  echo SVG Text:
-  echo $svg_text1
-fi
-
-# Generate a splash screen (must be PNG, must be < 13k to display on Balena)
-template=$(dirname $0)/lcd-480x320-splash.svg
-svg_text2=$(generate_svg_text)
-echo $svg_text2 > $(dirname $0)/lcd-startup.svg
-echo $svg_text2 | rsvg-convert - -o $(dirname $0)/lcd-startup.png
-if [ ! "$?" = 0 ]
-then
-  echo SVG Text:
-  echo $svg_text2
-fi
-
-# BalenaCloud requires the startup image to be 13.09kb or less
-# so we need to minimize that image
-optipng $(dirname $0)/lcd-startup.png --clobber -o8 -out $(dirname $0)/lcd-startup-min.png
-ls -l $(dirname $0)/*.png
-
-"""
