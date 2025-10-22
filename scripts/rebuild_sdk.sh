ea8d8cb17a9e0ee042ca604d4922ca0d5b2ecf27
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-10-22 16:35:27 +0800

    Updating Balena CLI

--- scripts/rebuild_sdk.sh
+++ scripts/rebuild_sdk.sh
@@ -181,7 +181,7 @@ if [ "$osname" = "Linux" ]
 then
   jdk21_url=https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz
   jdk25_url=https://download.java.net/java/GA/jdk25/bd75d5f9689641da8e1daabeccb5528b/36/GPL/openjdk-25_linux-x64_bin.tar.gz
-  balena_cli_url=https://github.com/balena-io/balena-cli/releases/download/v22.4.9/balena-cli-v22.4.9-linux-x64-standalone.tar.gz
+  balena_cli_url=https://github.com/balena-io/balena-cli/releases/download/v22.4.15/balena-cli-v22.4.15-linux-x64-standalone.tar.gz
   android_cltools_url=https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip
   lua_make_target=linux
 elif [ "$osname" = "Darwin" ]
@@ -189,7 +189,7 @@ then
   # For now, Intel binaries are preferred to those for Apple Silicon
   jdk21_url=https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_macos-x64_bin.tar.gz
   jdk25_url=https://download.java.net/java/GA/jdk25/bd75d5f9689641da8e1daabeccb5528b/36/GPL/openjdk-25_macos-x64_bin.tar.gz
-  balena_cli_url=https://github.com/balena-io/balena-cli/releases/download/v22.4.9/balena-cli-v22.4.9-macOS-x64-standalone.tar.gz
+  balena_cli_url=https://github.com/balena-io/balena-cli/releases/download/v22.4.15/balena-cli-v22.4.15-macOS-x64-standalone.tar.gz
   android_cltools_url=https://dl.google.com/android/repository/commandlinetools-mac-13114758_latest.zip
   lua_make_target=macosx
 fi
