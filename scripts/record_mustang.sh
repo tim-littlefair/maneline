ba282587096b97a56456397bd0c61ae6ed036192
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-11-17 15:17:58 +0800

    Detection of audio card number implemented

--- scripts/record_mustang.sh
+++ scripts/record_mustang.sh
@@ -4,6 +4,21 @@
 # TODO: device name presently hard-coded, should
 # be detected.
 
+set_device_name_linux()
+{
+  proc_card_line=$(grep Mustang /proc/asound/cards | head -1)
+  if [ -z "$proc_card_line" ]
+  then
+    echo No Mustang device found
+    exit 1
+  fi
+  card_number_string=$(echo $proc_card_line|cut -c1-2)
+  card_number=$(($card_number_string))
+  device_name=hw:$card_number,0
+  echo Detected card details: $proc_card_line
+  echo Recording from device $device_name
+}
+
 handle_ctrl_c()
 {
   echo Stopping arecord
@@ -11,12 +26,21 @@ handle_ctrl_c()
 }
 trap handle_ctrl_c INT
 
+if [ "$(uname)" = "Linux" ]
+then
+  set_device_name_linux
+else
+  echo Recording presently only supported on Linux
+  exit 2
+fi
+
 start_time=$(date +%s)
 wav_filename=$start_time.wav
-arecord --device="hw:3,0" -f dat --channels=2 --format=s16_le --rate=48000 > $wav_filename
+arecord --device="$device_name" --channels=2 --format=S16_LE --rate=48000 > $wav_filename
 end_time=$(date +%s)
+
 duration=$(($end_time-$start_time))
-flac_filename=$start_time_$duration.flac
+flac_filename=$start_time-$duration.flac
 flac $wav_filename -o $flac_filename
 
 ls -l $start_time*
