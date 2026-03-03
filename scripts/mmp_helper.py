ac386e290c54adf37a07b83b3149d113b66fe960
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-03-03 17:07:44 +0800

    Connect and service resolution succeeding

--- scripts/mmp_helper.py
+++ scripts/mmp_helper.py
@@ -33,17 +33,43 @@ async def main2():
     print("Device found by scanner:", device)
     await main(device)
 
+def resolve_services(device, client, advert):
+    for i in range(1,len(advert.service_uuids)):
+        new_service = bleak.backends.service.BleakGATTService(None, i, advert.service_uuids[i-1])
+        client.services.services[i] = new_service
+        if new_service.uuid == "90:55:95:80:b7:07:11:ee:ac:b1:7b:7e:30:f1:af:54":
+            mmp_request_characteristic = bleak.backends.characteristic.BleakGATTCharacteristic(
+                None, 0x1b, "82:0a:7e:34:4e:0a:4f:90:85:20:04:eb:ce:35:a3:a1",
+                properties=[],
+                max_write_without_response_size=None,
+                service=new_service
+            )
+            new_service.add_characteristic((mmp_request_characteristic))
+            client.service.characteristics[0x1b] = mmp_request_characteristic
+            client.characteristics[0x1b] = mmp_request_characteristic
+
+
 async def main3():
-    devices = await bleak.BleakScanner.discover(detection_callback=detection_callback)
-    for d in devices:
+    devices_and_adverts = await bleak.BleakScanner.discover(
+        return_adv=True
+    )
+    client = None
+    for d_and_a in devices_and_adverts.values():
+        d = d_and_a[0]
+        a = d_and_a[1]
         if d.name=="Mustang Micro Plus":
             print(d)
+            print(a)
             client = bleak.BleakClient(d, pair=True,timeout=20)
-            await client.connect()
-            print(client.is_connected)
-            print("Services: ", client.services.services)
-            print("Characteristics: ", client.services.characteristics)
-            await client.disconnect()
+            break
+
+    if client:
+        await client.connect()
+        print(client.is_connected)
+        # resolve_services(d, client, a)
+        print("Services: ", client.services.services)
+        print("Characteristics: ", client.services.characteristics)
+        await client.disconnect()
 
 asyncio.run(main3())
 
