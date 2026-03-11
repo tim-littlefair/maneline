e4fcb9cc83eea254c9e474d34e39bf89dde90ec3
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-03-11 16:30:22 +0800

    Exploratory code related to TinyB library
    Initial exploration suggests that this will not end up being useful

--- desktop-app/src/main/java/net/heretical_camelid/maneline/desktop_app/DeviceTransportTinyB.java
+++ desktop-app/src/main/java/net/heretical_camelid/maneline/desktop_app/DeviceTransportTinyB.java
@@ -7,10 +7,14 @@ import java.util.List;
 // /*
 import org.sputnikdev.bluetooth.URL;
 import org.sputnikdev.bluetooth.manager.AdapterDiscoveryListener;
+import org.sputnikdev.bluetooth.manager.BluetoothGovernor;
 import org.sputnikdev.bluetooth.manager.CharacteristicGovernor;
 import org.sputnikdev.bluetooth.manager.DeviceDiscoveryListener;
+import org.sputnikdev.bluetooth.manager.DeviceGovernor;
 import org.sputnikdev.bluetooth.manager.DiscoveredAdapter;
 import org.sputnikdev.bluetooth.manager.DiscoveredDevice;
+import org.sputnikdev.bluetooth.manager.GovernorListener;
+import org.sputnikdev.bluetooth.manager.ManagerListener;
 import org.sputnikdev.bluetooth.manager.impl.BluetoothManagerBuilder;
 // import org.sputnikdev.bluetooth.manager.transport.bluegiga.BluegigaFactory;
 import org.sputnikdev.bluetooth.manager.transport.tinyb.TinyBFactory;
@@ -19,7 +23,8 @@ import org.sputnikdev.bluetooth.manager.BluetoothManager;
 
 import tinyb.*;
 
-public class DeviceTransportTinyB implements IDeviceTransport {
+public class DeviceTransportTinyB
+    implements IDeviceTransport, ManagerListener, GovernorListener, DeviceDiscoveryListener, AdapterDiscoveryListener {
     @Override
     public int read(byte[] packetBuffer) {
         return 0;
@@ -43,37 +48,6 @@ public class DeviceTransportTinyB implements IDeviceTransport {
         System.out.print(" Connected = " + device.getConnected());
         System.out.println();
     }
-    /*
-    static BluetoothDevice getDevice(String address) throws InterruptedException {
-        boolean nativeLoadStatus = TinyBFactory.loadNativeLibraries();
-        System.out.println("nls="+nativeLoadStatus);
-        BluetoothManager mgr =  new BluetoothManagerBuilder()
-            .withTinyBTransport(true)
-            .build()
-        ;
-        BluetoothDevice sensor = null;
-        for (int i = 0; (i < 15) && running; ++i) {
-            List<BluetoothDevice> list = mgr.getDevices();
-            if (list == null)
-                return null;
-
-            for (BluetoothDevice device : list) {
-                printDevice(device);
-                /*
-                 * Here we check if the address matches.
-                 * /
-                if (device.getAddress().equals(address))
-                    sensor = device;
-            }
-
-            if (sensor != null) {
-                return sensor;
-            }
-            Thread.sleep(4000);
-        }
-        return null;
-    }
-    */
 
     static {
         try {
@@ -95,44 +69,41 @@ public class DeviceTransportTinyB implements IDeviceTransport {
         DeviceTransportTinyB instance = new DeviceTransportTinyB();
         System.out.println("TinyBFactory registered");
 
-        mgr.getCharacteristicGovernor(
-            new URL("/XX:XX:XX:XX:XX:XX/F7:EC:62:B9:CF:1F/"
-                + "0000180f-0000-1000-8000-00805f9b34fb/00002a19-0000-1000-8000-00805f9b34fb"
-            ),
-            true
-        )
-            .whenReady(CharacteristicGovernor::read)
-            .thenAccept(data -> {
-                System.out.println("Battery level: " + data[0]);
-            })
-        ;
-
-        /*
-        BluetoothManager manager = BluetoothManager.getBluetoothManager();
-        manager.addDeviceDiscoveryListener(instance);
-        manager.addAdapterDiscoveryListener(instance);
-        //String var0 = tinyb.BluetoothManager.getNativeAPIVersion();
-        String var1 = tinyb.BluetoothManager.class.getPackage().getSpecificationVersion();
-        System.out.println("sv="+var1);
-        manager.start(true);
-        String targetDeviceAddress = "84:17:15:2B:4E:7E";
-        BluetoothManager manager = BluetoothManager.getBluetoothManager();;
-        boolean discoveryStarted = manager.startDiscovery();
+        DeviceTransportTinyB theTransport = instance;
+        mgr.addManagerListener(theTransport);
+        mgr.addAdapterDiscoveryListener(theTransport);
+        mgr.addDeviceDiscoveryListener(theTransport);
+        DeviceGovernor dg = mgr.getDeviceGovernor(new URL("/XX:XX:XX:XX:XX:XX/84:17:15:2B:4E:7E"));
+        System.out.println(dg.getResolvedServices());
+        mgr.start(true);
+        String targetDeviceAddress = "";
         try {
-            BluetoothDevice sensor = getDevice(targetDeviceAddress);
-            manager.stopDiscovery();
-            if (sensor == null) {
-                System.err.println("No sensor found with the provided address.");
-                System.exit(-1);
-            }
-        } catch (BluetoothException e) {
-            System.err.println("Discovery could not be stopped.");
+            Thread.sleep(30000);
         } catch (InterruptedException e) {
-            System.err.println("Discovery was interrupted.");
+            throw new RuntimeException(e);
         }
-         */
+        mgr.dispose();
         System.exit(0);
     }
 
 
+    @Override
+    public void ready(BluetoothGovernor governor, boolean ready) {
+        System.err.println(governor.getURL());
+    }
+
+    @Override
+    public void ready(boolean isReady) {
+
+    }
+
+    @Override
+    public void discovered(DiscoveredDevice discoveredDevice) {
+        System.err.println(discoveredDevice.getName());
+    }
+
+    @Override
+    public void discovered(DiscoveredAdapter adapter) {
+        System.err.println(adapter.getName());
+    }
 }
