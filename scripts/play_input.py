2b515712bf62a9cbddf1e05639c9a7f3bea4162b
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-03-13 09:39:03 +0800

    WIP on gatttool/hcitool scripting engine

--- scripts/play_input.py
+++ scripts/play_input.py
@@ -7,17 +7,39 @@ import select
 import time
 import tty
 
-class GatttoolWrapper :
-    def __init__(self):
-        self.popen = subprocess.Popen(
-            args=["/usr/bin/gatttool", "-I" ],
-            bufsize=1,
+
+def hcitool_run(args_string, timeout=None):
+    return _tool_run("/usr/bin/hcitool")
+
+
+def gatttool_run(args_string, timeout=None):
+    return _tool_run("/usr/bin/gatttool -b 84:17:15:2B:4E:7E", args_string, timeout)
+
+
+def _tool_run(tool_cmd_prefix, args_string, timeout, only_start=False):
+    if timeout is None:
+        timeout = 10.0
+    if only_start is True:
+        return subprocess.Popen(
+            args=tool_cmd_prefix + " " + args_string,
+            shell=True,
             text=True,
-            stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT
+            stderr=subprocess.STDOUT,
+            timeout=timeout
+        )
+    else:
+        return subprocess.run(
+            args=tool_cmd_prefix + " " + args_string,
+            shell=True,
+            text=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            timeout=timeout
         )
 
+
+"""
     def read_lines(self, lines_to_read, per_read_timeout, max_read_attempts):
         lines_read = []
         for i in range(0,max_read_attempts):
@@ -31,45 +53,47 @@ class GatttoolWrapper :
             else:
                 print(".",flush=True,end='')
         return lines_read
+"""
 
 
-    def process_json_script(self, fn, read_delay=3.0):
-        cmd_array = json.load(open(fn))
-        for cmd in cmd_array:
+def process_script_section(section_name, timeout=None, only_start=False):
+    global _SCRIPT_SECTIONS
+    if timeout is not None:
+        pass
+    elif only_start is True:
+        timeout = 10.0
+    else:
+        timeout = 1.0
+    next_command = None
+    output_lines = None
+    for cmd in _SCRIPT_SECTIONS.get(section_name):
+        try:
             next_command = cmd.get("send", None)
             if next_command is None:
                 continue
-            lines_to_read = 2
+            print(f"Sending '{next_command}'", flush=True, end="")
+            result = gatttool_run(next_command)
+            output_lines = result.stdout.split("\n")
             if "expect_lines" in cmd:
-                lines_to_read=cmd.get("expect_lines")
-            expect_text = cmd.get("expect_text", next_command)
+                assert len(output_lines) == cmd.get("expect_lines")
             if "expect_text" in cmd:
-                print("Expecting:", expect_text)
-            print("Sending:", next_command)
-            print(next_command,file=self.popen.stdin,flush=True)
-            response_lines = self.read_lines(lines_to_read, read_delay, lines_to_read+5)
-            if expect_text == "":
-                print("\n".join(response_lines))
-                continue
+                expect_text = cmd.get("expect_text")
+                assert expect_text in "\n".join(output_lines)
+                print(f" ... found '{expect_text}'")
             else:
-                if expect_text in "\n".join(response_lines):
-                    print("Found:", expect_text)
-                    continue
-                one_more_line = self.read_lines(1,read_delay,2)
-                response_lines += one_more_line
-                if expect_text in "\n".join(response_lines):
-                    print("Found:", expect_text)
-                    continue
-            print("Not found:",expect_text)
+                print(f"\n{"\n".join(output_lines)}")
+        except AssertionError:
+            print(sys.exc_info()[1])
+            traceback.print_tb(sys.exc_info()[2])
+            print("\n".join(["", "Output:"] + output_lines))
             break
 
 
-
 if __name__ == "__main__":
-    gtw = GatttoolWrapper()
-    gtw.process_json_script(sys.argv[1],3.0)
-
-
+    global _SCRIPT_SECTIONS
+    _SCRIPT_SECTIONS = json.load(open(sys.argv[1]))
+    # process_script_section("connection", 5.0)
+    process_script_section("gatt-preamble-1", 5.0)
 
 """
 
