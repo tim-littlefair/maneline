90b2968ac1246c412b6603e44fe223b0f7fe22f7
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-06-05 16:33:26 +0800

    WIP on SD card script

---
+++ balena-sdcard/balena_sdcard_functions.sh
@@ -0,0 +1,291 @@
+#! /bin/bash
+# prepare_maneline_sdcard.sh
+
+usage() {
+    cat <<+
+
+Usage: $0 [balena device type] [WiFi_SSID] [WiFi_Password] { --hotspot }
+
+Script to preconfigure an SD card for usage with the Maneline app.
+The script will install a device driver overlay for the Waveshare 3.5"
+LCD screen for RPi, and will configure the Pi to either to connect to
+a local WiFi access point, or to offer a hotspot WiFi access point
+of its own.
+
+The following balena device types have been tested:
+    raspberrypi0-2w-64, raspberrypi3-64, raspberrypi4-64, raspberrypi5
+Other 64bit RPi models may or may not work.
+
+WiFi SID and password are required to make the device connectable.
+
+If the optional '--hotspot' argument is specified, the Balena device
+will act as a WiFi hotspot accepting connections with the SSID and
+password specified, otherwise it will attempt to connect to a local
+WiFi access point serving the specified SSID.
+
+If '--hotspot' is specified, the script will create a PNG matching the
+image basename which can be scanned to connect to the hotspot.
+
+This script only runs on Linux, and requires 'sudo' access to mount
+the disk image file retrieved from balena.io so that the filesystem
+changes required can be applied.
+
++
+}
+
+verify_linux_sudo_access() {
+    uname | grep -i Linux > /dev/null
+    if [ "$?" != "0" ]
+    then
+        echo "\nThis script only runs on Linux\n"
+        usage
+        exit 1
+    fi
+
+    sudo echo "sudo access verified"
+    if [ "$?" != "0" ]
+    then
+        echo "\nsudo access verification failed\n"
+        usage
+        exit 2
+    fi
+}
+
+verify_balena_cli() {
+    which balena > /dev/null
+    if [ "$?" != "0" ]
+    then
+        echo "\nbalena CLI executable not found\n"
+        usage
+        exit 3
+    fi
+
+}
+
+get_os() {
+    if [ -r $CACHE_DIR/$balena_device_type.img ]
+    then
+        echo cached copy of OS found - will be used
+    else
+        balena os download $balena_device_type -o $CACHE_DIR/$balena_device_type.img
+        if [ "$?" != "0" ]
+        then
+            echo "\nfailed to download OS\n"
+            usage
+            exit 3
+        fi
+    fi
+    cp -f $CACHE_DIR/$balena_device_type.img $WORKING_IMAGE_PATH
+    if [ "$?" = "0" ]
+    then
+        echo "working copy of image created"
+    else
+        echo "\nfailed to copy OS\n"
+        usage
+        exit 3
+    fi
+}
+
+configure_os() {
+    # args: ssid password { hotspot }
+    if [ ! -z "wifi_ssid_local" ]
+    then
+        cxn_fname=/tmp/local-wifi
+        cat > $cxn_fname <<+
+[connection]
+id=local-wifi
+priority=1
+type=wifi
+
+[wifi]
+hidden=true
+mode=infrastructure
+ssid=$wifi_ssid_local
+
+[ipv4]
+method=auto
+
+[ipv6]
+method=disabled
+
+[wifi-security]
+auth-alg=open
+key-mgmt=wpa-psk
+psk=$wifi_password_local
++
+    balena os configure $WORKING_IMAGE_PATH \
+        --fleet gh_tim_littlefair/maneline-fleet \
+        --device-type $balena_device_type \
+        --config-network ethernet \
+        --system-connection $cxn_fname \
+        --dev
+fi
+
+if [ ! -z "$wifi_ssid_hs" ]
+    then
+        cxn_fname=/tmp/balena-hotspot
+        cat > $cxn_fname <<+
+[connection]
+id=balena-hotspot
+priority=2
+uuid=36060c57-aebd-4ccf-aba4-ef75121b5f77
+type=wifi
+autoconnect=true
+interface-name=wlan0
+permissions=
+secondaries=
+
+[wifi]
+band=bg
+mac-address-blacklist=
+mac-address-randomization=0
+mode=ap
+seen-bssids=
+ssid=$wifi_ssid_hs
+
+[wifi-security]
+group=
+key-mgmt=wpa-psk
+pairwise=
+proto=rsn
+psk=$wifi_password_hs
+
+[ipv4]
+method=auto
+
+[ipv6]
+method=disabled
++
+    balena os configure $WORKING_IMAGE_PATH \
+        --fleet gh_tim_littlefair/maneline-fleet \
+        --device-type $balena_device_type \
+        --config-network ethernet \
+        --system-connection $cxn_fname \
+        --dev
+
+fi
+    config_outcome=$?
+    rm $cxn_fname
+    if [ "$config_outcome" = "0" ]
+    then
+        echo "OS image configured"
+    else
+        echo "\nfailed to configure OS\n"
+        exit 4
+    fi
+    balena preload $WORKING_IMAGE_PATH --fleet gh_tim_littlefair/maneline-fleet \
+        --commit current
+    if [ "$?" = "0" ]
+    then
+        echo "preloaded application"
+    else
+        echo "\nfailed to preload application\n"
+        exit 5
+    fi
+
+}
+
+install_overlay() {
+    partition1_params=$(fdisk -l $WORKING_IMAGE_PATH | sed -e 's/*//' | grep img1)
+    start_sector=$(echo $partition1_params | ( read a b c d e f g h; echo $b ))
+    sector_count=$(echo $partition1_params | ( read a b c d e f g h; echo $d ))
+    sudo mount -t vfat \
+        -o loop,rw,offset=$(($start_sector*512)),sizelimit=$(($sector_count*512)) \
+        $WORKING_IMAGE_PATH $MOUNT_DIR
+    sudo cp lcd-driver/tft35a.dtbo $MOUNT_DIR/overlays
+    sudo cp lcd-driver/config.txt $MOUNT_DIR
+    sleep 1
+    sudo sync ; sudo sync
+    sleep 1
+    sudo umount $MOUNT_DIR
+    mv -f $WORKING_IMAGE_PATH $OUTPUT_IMAGE_PATH
+}
+
+flash_to_sdcard() {
+    drive=/dev/mmcblk0
+    balena util available-drives | grep $drive > /dev/null
+    flashing_cmd="balena os initialize $OUTPUT_IMAGE_PATH --type $balena_device_type --drive $drive"
+    if [ "$?" = "0" ]
+    then
+        echo "flashing to $drive"
+        $flashing_cmd
+        echo Additional SD cards can be initialized using the following command:
+        echo $flashing_cmd
+    else
+        echo "\n$drive not present - manually flash using the following command:"
+        echo $flashing_cmd
+    fi
+}
+
+balena_device_type=$1
+if [ -z "$balena_device_type" ]
+then
+    usage
+    exit 91
+fi
+
+if [ "$2" = "--env_ssids" ]
+then
+    wifi_ssid_hs=$WIFI_SSID_HS
+    wifi_password_hs=$WIFI_PASSWORD_HS
+    wifi_ssid_local=$WIFI_SSID_LOCAL
+    wifi_password_local=$WIFI_PASSWORD_LOCAL
+else
+    if [ -z "$4" ]
+    then
+       opt_hotspot_switch=$4
+       wifi_ssid_hs=$2
+       wifi_password_hs=$3
+    else
+        wifi_ssid_local=$2
+        wifi_password_local=$3
+    fi
+fi
+
+
+
+CACHE_DIR=_work/cache
+OUTPUT_DIR=_work/output
+MOUNT_DIR=_work/mount
+for d in $CACHE_DIR $OUTPUT_DIR $MOUNT_DIR
+do
+    if [ -d "$d" ]
+    then
+        echo $(basename $d) directory found
+    else
+        mkdir -p $d
+        if [ "$?" = "0" ]
+        then
+            echo $(basename $d) directory created at $d
+        else
+            echo "\nfailed to create $(basename $d) directory at $d\n"
+            exit 92
+        fi
+    fi
+done
+
+WORKING_IMAGE_PATH=/tmp/maneline-$wifi_ssid-$balena_device_type.img
+OUTPUT_IMAGE_PATH=$OUTPUT_DIR/maneline-$wifi_ssid-$balena_device_type.img
+for f in $WORKING_IMAGE_PATH $OUTPUT_IMAGE_PATH
+do
+    if [ -f $f ]
+    then
+        rm -f $f
+    fi
+done
+
+verify_linux_sudo_access
+verify_balena_cli
+get_os
+configure_os
+install_overlay
+flash_to_sdcard
+
+exit 0
+
+
+
+
+
+
+
